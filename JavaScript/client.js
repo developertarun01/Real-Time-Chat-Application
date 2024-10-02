981d49de8f54b3a41beb29615a2da9dd4a7cf5b9
developertarun01 <tarunbusinessmail@gmail.com>
2024-10-02 15:14:45 +0530

    first commit

---
+++ JavaScript/client.js
@@ -0,0 +1,53 @@
+const socket = io('http://localhost:8000');
+
+const form = document.getElementById('send-container');
+const messageInput = document.getElementById('messageInp')
+const messageContainer = document.querySelector('.container')
+var audio = new Audio('ding-sound-effect_2.mp3')
+
+const append = (message, position) =>{
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+    if(position == 'left') {
+        audio.play();
+    }
+}
+
+form.addEventListener(`submit`, (e) =>{
+    e.preventDefault();
+    const message = messageInput.value;
+    append(`You: ${message}`, 'right');
+    socket.emit('send', message);
+    messageInput.value = ''
+})
+
+const name = prompt("Enter your Name to Join")
+socket.emit('new-user-joined', name);
+
+const me = document.getElementById('me');
+me.innerHTML = name 
+
+socket.on('user-joined', name =>{
+    append(`${name} joined the Chat !`, 'left')
+    append1(", " +`${name}`)
+})
+
+socket.on('recieve', data =>{
+    append(`${data.name}: ${data.message}`, 'left')
+})
+
+socket.on('left', name =>{
+    append(`${name} left the Chat !`, 'left')
+    append1('')
+})
+
+const nameContainer = document.querySelector('.online')
+const append1 = (name) =>{
+    const nameElement = document.createElement('div');
+    nameElement.innerText = name;
+    nameElement.classList.add('name');
+    nameContainer.append(nameElement);
+}
---
+++ nodeServer/index.js
@@ -0,0 +1,26 @@
+const io = require('socket.io')(8000)
+const users = {};
+
+const express = require('express')
+const cors = require('cors')
+const app = express();
+
+app.use(cors());
+
+io.on('connection', socket =>{
+    socket.on('new-user-joined', name =>{
+        // console.log("Working")
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name)
+    });
+
+    socket.on('send', message =>{
+        socket.broadcast.emit('recieve', {message: message, name: users[socket.id]})
+    });
+
+    socket.on('disconnect', message =>{
+        socket.broadcast.emit('left', users[socket.id])
+        delete users[socket.id];
+    });
+
+})
